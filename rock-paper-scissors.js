27098dca9c6c605dd222da84b8c559f90a732344
copaiement <cole.paiement@gmail.com>
2023-02-13 21:34:16 -0800

    Final updates to game code, html and css

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,20 +3,15 @@
 // simple rock paper scissors game built for TOP: Fundamentals
 
 // initialize global variables
-let playerScore = 0;
+let playerScore = 4;
 let computerScore = 0;
 let playerSelection;
 let computerSelection;
-let roundNumber = 0;
+let roundNumber = 1;
 let roundResults;
 
 // initialize inputs and outputs
 const gameTiles = document.querySelectorAll('.game-tile');
-const round = document.getElementById("round");
-const rules = document.getElementById("rules");
-const msg = document.getElementById("msg");
-const playerTotalMsg = document.getElementById("player-total");
-const computerTotalMsg = document.getElementById("computer-total");
 
 // Get button press to start a round
 gameTiles.forEach(gameTile => {
@@ -32,29 +27,79 @@ gameTiles.forEach(gameTile => {
         roundResults = playRound(computerSelection, playerSelection);
 
         // update results
-        if (playerScore == 5 || computerScore == 5) {
-            gameOver(playerScore, computerScore);
-        } else {
-            playerScore += roundResults.playerScore;
-            computerScore += roundResults.computerScore;
-            roundNumber += 1;
-        }
+        playerScore += roundResults.playerScore;
+        computerScore += roundResults.computerScore;
+        roundNumber += 1;
+
         // print out results
+        const round = document.getElementById("round");
         round.textContent = `Round: ${roundNumber}`;
+        const msg = document.getElementById("msg");
         msg.textContent = roundResults.msg;
+        const playerTotalMsg = document.getElementById("player-total");
+        const computerTotalMsg = document.getElementById("computer-total");
         playerTotalMsg.textContent = `Player Score: ${playerScore}`;
         computerTotalMsg.textContent = `Computer Score: ${computerScore}`;
 
+        if (playerScore == 5 || computerScore == 5) {
+            gameOver(playerScore, computerScore);
+        }
+    
     });
 });
 
 // Game Over Function
-function gameOver(){
-    
+// Turn all divs off
+function gameOver(playerScore, computerScore){
+    const allDivs = document.querySelectorAll("div");
+    allDivs.forEach(div => {
+        div.style.display = "none";
+    });
+    //Turn game over divs on
+    const gameOverDivs = document.querySelectorAll(".game-over");
+    gameOverDivs.forEach(gameOverDiv => {
+        gameOverDiv.style.display = "block";
+    });
+    //Output Final Scores
+    const GoPlayerTotal = document.getElementById("go-player-total");
+    const GoComputerTotal = document.getElementById("go-computer-total");
+    GoPlayerTotal.textContent = `Player Score: ${playerScore}`;
+    GoComputerTotal.textContent = `Computer Score: ${computerScore}`;
+
+    const msg = document.getElementById("go-msg");
+    if (playerScore < computerScore) {
+        msg.textContent = "YOU LOSE";
+    } else {
+        msg.textContent = "YOU WIN";
+    }
+}
+
+// Reset the game
+function reset() {
+    const allDivs = document.querySelectorAll("div");
+    allDivs.forEach(div => {
+        div.style.display = "";
+    });
+    //Turn game over divs on
+    const gameOverDivs = document.querySelectorAll(".game-over");
+    gameOverDivs.forEach(gameOverDiv => {
+        gameOverDiv.style.display = "none";
+    });
+
+    playerScore = 0;
+    computerScore = 0;
+    roundNumber = 1;
+    const round = document.getElementById("round");
+    round.textContent = `Round: ${roundNumber}`;
+    const playerTotalMsg = document.getElementById("player-total");
+    const computerTotalMsg = document.getElementById("computer-total");
+    playerTotalMsg.textContent = `Player Score: ${playerScore}`;
+    computerTotalMsg.textContent = `Computer Score: ${computerScore}`;
 }
 
 // Show or hide the rules
 function showRules() {
+    const rules = document.getElementById("rules");
     let buttonText = document.getElementById("btn-rules");
     if (rules.style.display === "none") {
         rules.style.display = "block";
